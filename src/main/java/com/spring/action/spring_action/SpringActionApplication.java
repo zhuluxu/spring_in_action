399a8134b458d356468cda7115b33a51b41e6a71
nignmengcc <1208931394@qq.com>
2022-10-01 16:50:44 +0800

    init

---
+++ src/main/java/com/spring/action/spring_action/SpringActionApplication.java
@@ -0,0 +1,13 @@
+package com.spring.action.spring_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/action/spring_action/SpringActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.action.spring_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
